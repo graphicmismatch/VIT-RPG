0d8937406692a0124d21bf107b1641a8715373b7
graphicmismatch <riptide.syncopate@gmail.com>
2024-07-19 01:43:20 +0530

    feat:  added player movement using rigidbody

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    protected Vector2 movement;
+    private Vector2 direction;
+    private Rigidbody2D rb;
+    private float centeringTimer;
+    private bool centering;
+    private Vector2 centeringTarget;
+    private Vector2 centeringOrigin;
+    private bool feasibleCenteringPath;
+
+
+    public float speed;
+    public LayerMask wallLayers;
+    public float centeringTime;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        centeringTimer = 0f;
+        centering = false;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = movement * speed;
+        if (Mathf.Approximately(movement.sqrMagnitude, 0))
+        {
+            if (!centering)
+            {
+                centering = true;
+                centeringTarget = new Vector2(((direction.x > 0) ? Mathf.CeilToInt(this.transform.position.x) : Mathf.FloorToInt(this.transform.position.x)), ((direction.y > 0) ? Mathf.CeilToInt(this.transform.position.y) : Mathf.FloorToInt(this.transform.position.y)));
+                direction = Vector2.zero;
+                centeringOrigin = this.transform.position;
+                feasibleCenteringPath = Physics2D.LinecastAll(centeringOrigin, centeringTarget, wallLayers).Length == 0;
+            }
+            if (feasibleCenteringPath)
+            {
+                
+                this.transform.position = Vector2.Lerp(centeringOrigin, centeringTarget, centeringTimer / centeringTime);
+               
+            }
+            if (centeringTimer <= centeringTime)
+            {
+                centeringTimer += Time.deltaTime;
+            }
+        }
+        else {
+            centering = false;
+            centeringTimer = 0;
+        }
+    }
+    public void OnMove(InputValue ctx) {
+        if (ctx.Get() != null)
+        {
+            direction = ((Vector2)ctx.Get());
+            movement = ((Vector2)ctx.Get()).normalized;
+        }
+        else {
+            movement = Vector2.zero;
+        }
+    }
+}
