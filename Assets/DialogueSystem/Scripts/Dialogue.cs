8c6fd86106f401b71c07110c836bb29eb4ec7c45
graphicmismatch <riptide.syncopate@gmail.com>
2024-07-24 00:24:12 +0530

    feat: added temporary dialogue system

---
+++ Assets/DialogueSystem/Scripts/Dialogue.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+[System.Serializable]
+public struct Options
+{
+    public string optName;
+    public Dialogue next;
+}
+
+[System.Serializable]
+public struct Character
+{
+    public string charName;
+    public Sprite cha;
+    public bool facingLeft;
+}
+
+[CreateAssetMenu(fileName = "Dialogue", menuName = "ScriptableObjects/DialogueSystem/Dialogue", order = 5)]
+public class Dialogue : ScriptableObject
+{
+    public Character Left;
+    public Character Right;
+    public bool isLeftSpeaking;
+    public float timeBetweenCharacters;
+    public string[] lines;
+    public string[] defResponses;
+    public AudioClip[] clips;
+    public Options[] opt;
+    
+}
